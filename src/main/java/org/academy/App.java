91f8a0c4841101f4538ba93ab19587c228d8938f
Stanislav Hrynus <stanislav.hrynus@gmail.com>
2024-01-17 19:27:24 +0200

    realized solution for kata to square(root) or not to square(root)

--- src/main/java/org/academy/App.java
+++ src/main/java/org/academy/App.java
@@ -1,12 +1,11 @@
 package org.academy;
 
-import org.academy.kata.implementation.s_hrynus.EightImpl;
-
-import java.util.Arrays;
-
+/**
+ * Hello world!
+ */
 public class App {
     public static void main(String[] args) {
-        EightImpl eight = new EightImpl();
-        System.out.println(Arrays.toString(eight.squareOrSquareRoot(new int[]{4, 3, 9, 7, 2, 1})));
+
+        System.out.println("Hello World!");
     }
 }
