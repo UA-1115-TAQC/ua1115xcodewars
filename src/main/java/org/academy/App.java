9ac84d65b6b1486fb00e7a24f4d97f974512b1be
Mariana Gogashvili <marianagogashvili24@gmail.com>
2024-01-17 15:33:13 +0200

    implemented getVolumeOfCuboid in EightImpl

--- src/main/java/org/academy/App.java
+++ src/main/java/org/academy/App.java
@@ -1,11 +1,13 @@
 package org.academy;
 
+import org.academy.kata.implementation.hohashvili.EightImpl;
+
 /**
  * Hello world!
  */
 public class App {
     public static void main(String[] args) {
-
-        System.out.println("Hello World!");
+        EightImpl eightImpl = new EightImpl();
+        System.out.println(eightImpl.getVolumeOfCuboid(12, 10, 5));
     }
 }
