6058d3fb97b56e63eb2fdddfa7f1c8c0c3b7c807
Anastasiia <anastasiiak55@gmail.com>
2024-01-18 00:53:18 +0200

    solved task Count of positive/sum of negative

--- src/main/java/org/academy/kata/implementation/nasock/EightImpl.java
+++ src/main/java/org/academy/kata/implementation/nasock/EightImpl.java
@@ -20,7 +20,20 @@ public class EightImpl implements Eight {
     }
 
     public int[] countPositivesSumNegatives(int[] input) {
-        return new int[0];
+        if(input == null || input.length == 0){
+            return new int[]{};
+        }
+
+        int count = 0;
+        int sum = 0;
+        for(int num : input){
+            if (num > 0){
+                count++;
+            } else if (num < 0){
+                sum = sum + num;
+            }
+        }
+        return new int[]{count, sum};
     }
 
     public int stringToNumber(String str) {
@@ -38,4 +51,5 @@ public class EightImpl implements Eight {
     public boolean am_i_wilson(double n) {
         return false;
     }
+
 }
