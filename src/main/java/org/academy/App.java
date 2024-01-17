05421fc58277c5df05aa057a47c655047d827aea
Mariana Gogashvili <marianagogashvili24@gmail.com>
2024-01-17 17:58:12 +0200

    implemented balance in SixImpl

--- src/main/java/org/academy/App.java
+++ src/main/java/org/academy/App.java
@@ -1,11 +1,15 @@
 package org.academy;
 
+import org.academy.kata.implementation.hohashvili.SixImpl;
+
 /**
  * Hello world!
  */
 public class App {
     public static void main(String[] args) {
-
-        System.out.println("Hello World!");
+        String test = "1000.00\n125 Market 125.45\n126 Hardware 34.95\n127 Video 7.45\n128 Book 14.32 " +
+                "\n129 Gasoline 16.10";
+        SixImpl sixImpl = new SixImpl();
+        System.out.println(sixImpl.balance(test));
     }
 }
