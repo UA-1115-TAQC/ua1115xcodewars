14454b4cbbfb0820b54b934c44ddc6fb4c6c4838
Stanislav Hrynus <stanislav.hrynus@gmail.com>
2024-01-18 01:25:52 +0200

    realized solution for kata help the bookseller

--- src/main/java/org/academy/kata/implementation/s_hrynus/SixImpl.java
+++ src/main/java/org/academy/kata/implementation/s_hrynus/SixImpl.java
@@ -28,6 +28,25 @@ public class SixImpl implements Six {
     }
 
     public String stockSummary(String[] lstOfArt, String[] lstOf1stLetter) {
-        return null;
+        if (lstOf1stLetter.length == 0 || lstOfArt.length == 0) {
+            return "";
+        }
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < lstOf1stLetter.length; i++) {
+            int sum = 0;
+            for (String arts : lstOfArt) {
+                String[] values = arts.split(" ");
+                if (lstOf1stLetter[i].equals(String.valueOf(values[0].charAt(0)))) {
+                    sum += Integer.parseInt(values[1]);
+                }
+            }
+            stringBuilder.append("(")
+                    .append(lstOf1stLetter[i])
+                    .append(" : ")
+                    .append(sum)
+                    .append(")")
+                    .append(" - ");
+        }
+        return stringBuilder.substring(0, stringBuilder.length() - 3);
     }
 }
