e27ee604f5998952794616ab17bd98e458739db6
Mariana Gogashvili <marianagogashvili24@gmail.com>
2024-01-17 19:28:09 +0200

    implemented stockSummary in SixImpl

--- src/main/java/org/academy/App.java
+++ src/main/java/org/academy/App.java
@@ -1,11 +1,15 @@
 package org.academy;
 
+import org.academy.kata.implementation.hohashvili.SixImpl;
+
 /**
  * Hello world!
  */
 public class App {
     public static void main(String[] args) {
-
-        System.out.println("Hello World!");
+        String[] stock = {"ABART 20", "CDXEF 50", "BKWRK 25", "BTSQZ 89", "DRTYM 60"};
+        String[] letters =  {"A", "B", "C", "W"};
+        SixImpl sixImpl = new SixImpl();
+        System.out.println(sixImpl.stockSummary(stock, letters));
     }
 }
