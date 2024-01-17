db75dbd33d23e2c6f483627f63df010e40672e1b
Mariana Gogashvili <marianagogashvili24@gmail.com>
2024-01-17 16:55:04 +0200

    implemented newAvg in SevenImpl

--- src/main/java/org/academy/App.java
+++ src/main/java/org/academy/App.java
@@ -1,11 +1,20 @@
 package org.academy;
 
+import org.academy.kata.implementation.hohashvili.SevenImpl;
+
 /**
  * Hello world!
  */
 public class App {
     public static void main(String[] args) {
-
-        System.out.println("Hello World!");
+        SevenImpl sevenImpl = new SevenImpl();
+        double[] test = {14, 30, 5, 7, 9, 11, 15};
+        try {
+            System.out.println(sevenImpl.newAvg(test, 30));
+            System.out.println(sevenImpl.newAvg(test, 92));
+            System.out.println(sevenImpl.newAvg(test, 2));
+        } catch (IllegalArgumentException e) {
+            System.out.println(e);
+        }
     }
 }
--- src/main/java/org/academy/kata/implementation/hohashvili/SevenImpl.java
+++ src/main/java/org/academy/kata/implementation/hohashvili/SevenImpl.java
@@ -2,9 +2,17 @@ package org.academy.kata.implementation.hohashvili;
 
 import org.academy.kata.Seven;
 
+import java.util.Arrays;
+
 public class SevenImpl implements Seven {
     public long newAvg(double[] arr, double navg) {
-        return 0;
+        double sum = Arrays.stream(arr).sum();
+        double initLength = arr.length;
+        double result = navg*(initLength+1)-sum;
+        if (result <= 0) {
+            throw new IllegalArgumentException("Invalid 'navg' parameter - too small");
+        }
+        return (long) result;
     }
 
     public String seriesSum(int n) {
