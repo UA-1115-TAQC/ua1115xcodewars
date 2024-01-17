261ee166626b67d1702f58701df7af61bc89add8
Mariana Gogashvili <marianagogashvili24@gmail.com>
2024-01-17 16:25:16 +0200

    implemented TwoDecimalPlaces in EightImpl

--- src/main/java/org/academy/App.java
+++ src/main/java/org/academy/App.java
@@ -1,11 +1,14 @@
 package org.academy;
 
+import org.academy.kata.implementation.hohashvili.EightImpl;
+
 /**
  * Hello world!
  */
 public class App {
     public static void main(String[] args) {
-
-        System.out.println("Hello World!");
+        EightImpl eightImpl = new EightImpl();
+        System.out.println(eightImpl.TwoDecimalPlaces(5.5589));
+        System.out.println(eightImpl.TwoDecimalPlaces(3.3424));
     }
 }
