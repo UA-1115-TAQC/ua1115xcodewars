fab34189ea8ed569a72befd924df860a3de51db2
Mariana Gogashvili <marianagogashvili24@gmail.com>
2024-01-17 17:18:55 +0200

    implemented findNb in SixImpl

--- src/main/java/org/academy/kata/implementation/hohashvili/SixImpl.java
+++ src/main/java/org/academy/kata/implementation/hohashvili/SixImpl.java
@@ -4,7 +4,15 @@ import org.academy.kata.Six;
 
 public class SixImpl implements Six {
     public long findNb(long m) {
-        return 0;
+        int i = 1;
+        while (m > 0) {
+            m -= (long) Math.pow(i, 3);
+            i++;
+        }
+        if (m == 0) {
+            return i-1;
+        }
+        return -1;
     }
 
     public String balance(String book) {
