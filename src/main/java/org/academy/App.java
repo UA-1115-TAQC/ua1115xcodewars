cdb0fe2a0250e6c4cabc004c2110b93e9b18a7df
Mariana Gogashvili <marianagogashvili24@gmail.com>
2024-01-17 16:20:31 +0200

    implemented am_i_wilson in EightImpl

--- src/main/java/org/academy/App.java
+++ src/main/java/org/academy/App.java
@@ -1,11 +1,16 @@
 package org.academy;
 
+import org.academy.kata.implementation.hohashvili.EightImpl;
+
 /**
  * Hello world!
  */
 public class App {
     public static void main(String[] args) {
-
-        System.out.println("Hello World!");
+        EightImpl eightImpl = new EightImpl();
+        System.out.println(eightImpl.am_i_wilson(5));
+        System.out.println(eightImpl.am_i_wilson(13));
+        System.out.println(eightImpl.am_i_wilson(563));
+        System.out.println(eightImpl.am_i_wilson(10));
     }
 }
