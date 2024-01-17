26de2f934a009aa085358a1a14e7b99d9e24a383
Mariana Gogashvili <marianagogashvili24@gmail.com>
2024-01-17 15:41:16 +0200

    implemented mpgToKPM in EightImpl

--- src/main/java/org/academy/App.java
+++ src/main/java/org/academy/App.java
@@ -1,11 +1,14 @@
 package org.academy;
 
+import org.academy.kata.implementation.hohashvili.EightImpl;
+
 /**
  * Hello world!
  */
 public class App {
     public static void main(String[] args) {
-
-        System.out.println("Hello World!");
+        EightImpl eightImpl = new EightImpl();
+        System.out.println(eightImpl.mpgToKPM(19));
+        System.out.println(eightImpl.mpgToKPM(100));
     }
 }
