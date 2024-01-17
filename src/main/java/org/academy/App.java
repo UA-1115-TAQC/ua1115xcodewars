2bfc207570e0cb52269ed399d55e7ddbcbdfee41
Mariana Gogashvili <marianagogashvili24@gmail.com>
2024-01-17 19:42:56 +0200

    test

--- src/main/java/org/academy/App.java
+++ src/main/java/org/academy/App.java
@@ -7,8 +7,11 @@ import org.academy.kata.implementation.hohashvili.SixImpl;
  */
 public class App {
     public static void main(String[] args) {
-        String[] stock = {"ABART 20", "CDXEF 50", "BKWRK 25", "BTSQZ 89", "DRTYM 60"};
-        String[] letters =  {"A", "B", "C", "W"};
+
+//        String[] stock = {"ABART 20", "CDXEF 50", "BKWRK 25", "BTSQZ 89", "DRTYM 60"};
+        String[] stock = {"BBAR 150", "CDXE 515", "BKWR 250", "BTSQ 890", "DRTY 600"};
+//        String[] letters =  {"A", "B", "C", "W"};
+        String[] letters =  {"A", "B", "C", "D"};
         SixImpl sixImpl = new SixImpl();
         System.out.println(sixImpl.stockSummary(stock, letters));
     }
