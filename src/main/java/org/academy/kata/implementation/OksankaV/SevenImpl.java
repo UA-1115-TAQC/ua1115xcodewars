35f7e0131627f9330442157e68acf74b0ed58ec6
Oksanka Syrota <oksanochka.v@gmail.com>
2024-01-17 20:58:38 +0200

    Added a solution for Looking_for_a_benefactor task

--- src/main/java/org/academy/kata/implementation/OksankaV/SevenImpl.java
+++ src/main/java/org/academy/kata/implementation/OksankaV/SevenImpl.java
@@ -1,10 +1,14 @@
 package org.academy.kata.implementation.OksankaV;
 
 import org.academy.kata.Seven;
+import java.util.Arrays;
 
 public class SevenImpl implements Seven {
     public long newAvg(double[] arr, double navg) {
-        return 0;
+        double sumArr = Arrays.stream(arr).sum();
+        double result = navg * (arr.length + 1) - sumArr;
+        if (result <= 0) throw new IllegalArgumentException();
+        return (long) Math.ceil(result);
     }
 
     public String seriesSum(int n) {
