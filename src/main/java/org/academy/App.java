e1d6059da535c6ad428227f7b9858ebc7dcd8f35
Mariana Gogashvili <marianagogashvili24@gmail.com>
2024-01-17 17:03:52 +0200

    implemented seriesSum in SevenImpl

--- src/main/java/org/academy/App.java
+++ src/main/java/org/academy/App.java
@@ -1,11 +1,15 @@
 package org.academy;
 
+import org.academy.kata.implementation.hohashvili.SevenImpl;
+
 /**
  * Hello world!
  */
 public class App {
     public static void main(String[] args) {
-
-        System.out.println("Hello World!");
+        SevenImpl sevenImpl  = new SevenImpl();
+        System.out.println(sevenImpl.seriesSum(1));
+        System.out.println(sevenImpl.seriesSum(2));
+        System.out.println(sevenImpl.seriesSum(5));
     }
 }
