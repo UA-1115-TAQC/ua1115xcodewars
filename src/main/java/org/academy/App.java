72da8e89e5602dcf39b0440b2029516bb420dfd3
Mariana Gogashvili <marianagogashvili24@gmail.com>
2024-01-17 15:56:33 +0200

    implemented squareOrSquareRoot in EightImpl

--- src/main/java/org/academy/App.java
+++ src/main/java/org/academy/App.java
@@ -1,11 +1,22 @@
 package org.academy;
 
+import org.academy.kata.implementation.hohashvili.EightImpl;
+
 /**
  * Hello world!
  */
 public class App {
     public static void main(String[] args) {
+        System.out.println(String.valueOf(Math.ceil(5.0)));
+        System.out.println(String.valueOf(Math.ceil(5.1)));
 
-        System.out.println("Hello World!");
+        EightImpl eightImpl = new EightImpl();
+        int[] testArray = {4, 3, 9, 7, 2, 1};
+        int [] result = eightImpl.squareOrSquareRoot(testArray);
+        for (int r: result) {
+            System.out.print(r);
+            System.out.print(',');
+        }
+
     }
 }
