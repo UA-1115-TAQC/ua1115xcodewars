e918c768bc6003274f8ed8ab9a31a9b64873637e
Mariana Gogashvili <marianagogashvili24@gmail.com>
2024-01-17 16:13:16 +0200

    implemented stringToNumber in EightImpl

--- src/main/java/org/academy/App.java
+++ src/main/java/org/academy/App.java
@@ -1,11 +1,15 @@
 package org.academy;
 
+import org.academy.kata.implementation.hohashvili.EightImpl;
+
 /**
  * Hello world!
  */
 public class App {
     public static void main(String[] args) {
-
-        System.out.println("Hello World!");
+        EightImpl eightImpl = new EightImpl();
+        System.out.println(eightImpl.stringToNumber("1234"));
+        System.out.println(eightImpl.stringToNumber("-7"));
+        System.out.println(eightImpl.stringToNumber("0"));
     }
 }
