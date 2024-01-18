988f875866eae538f7a27253fa302120733e8416
Anastasiia <anastasiiak55@gmail.com>
2024-01-18 22:58:40 +0200

    solved task Sum of the firsth nth terms of series

--- src/main/java/org/academy/kata/implementation/nasock/SevenImpl.java
+++ src/main/java/org/academy/kata/implementation/nasock/SevenImpl.java
@@ -2,16 +2,24 @@ package org.academy.kata.implementation.nasock;
 
 import org.academy.kata.Seven;
 
+
 public class SevenImpl implements Seven {
     public long newAvg(double[] arr, double navg) {
         return 0;
     }
 
     public String seriesSum(int n) {
-        return null;
+        double sum = 0.0;
+        for (int i = 1; i <= n; i++){
+            double denom = (i-1) * 2 + i;
+            double num = 1/denom;
+            sum = sum + num;
+        }
+        return String.format("%.2f", sum);
     }
 
     public int whereIsHe(int p, int bef, int aft) {
         return 0;
     }
+
 }
