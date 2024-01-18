d26fa53d50b8f857eb41a9f0d4d6406d56742f2c
Anastasiia <anastasiiak55@gmail.com>
2024-01-18 21:25:26 +0200

    solved task Find numbers that are divisible by given number

--- src/main/java/org/academy/kata/implementation/nasock/EightImpl.java
+++ src/main/java/org/academy/kata/implementation/nasock/EightImpl.java
@@ -31,11 +31,37 @@ public class EightImpl implements Eight {
         return 0;
     }
 
+    private boolean isNatural(double num){
+        double decimalPart = num - (int)num;
+        return decimalPart == 0.0;
+    }
+
+    private int[] resizeArr(int[] arr){
+        int[] newArr = new int[arr.length * 2];
+        for (int i = 0; i < arr.length; i++){
+            newArr[i] = arr[i];
+        }
+        return newArr;
+    }
+
     public int[] divisibleBy(int[] numbers, int divider) {
-        return new int[0];
+        int[] result = new int[2];
+        int j = 0;
+        for(int num : numbers) {
+            double div = num/(double)divider;
+            if(isNatural(div)){
+                result[j] = num;
+                j++;
+                if(j > result.length - 1){
+                    result = resizeArr(result);
+                }
+            }
+        }
+        return result;
     }
 
     public boolean am_i_wilson(double n) {
         return false;
     }
+
 }
