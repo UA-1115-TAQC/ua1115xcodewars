40b3291658e2318ba9134b04af2ac726fc265366
Mariana Gogashvili <marianagogashvili24@gmail.com>
2024-01-18 12:54:08 +0200

    implemented f in SixImpl

--- src/main/java/org/academy/App.java
+++ src/main/java/org/academy/App.java
@@ -1,11 +1,17 @@
 package org.academy;
 
+import org.academy.kata.implementation.hohashvili.SixImpl;
+
 /**
  * Hello world!
  */
 public class App {
-    public static void main(String[] args) {
 
-        System.out.println("Hello World!");
+    public static void main(String[] args) {
+        SixImpl sixImpl = new SixImpl();
+        System.out.println(sixImpl.f(2.6e-08));// 1.29999999155e-08);
+        System.out.println(sixImpl.f(1.4e-09));// 6.999999997549999e-10
+        System.out.println(sixImpl.f(5.0e-06));//2.499996875007812e-06
+        System.out.println(sixImpl.f(2.4e-07));//1.1999999280000085e-07
     }
 }
--- src/main/java/org/academy/kata/implementation/hohashvili/FiveImpl.java
+++ src/main/java/org/academy/kata/implementation/hohashvili/FiveImpl.java
@@ -21,9 +21,39 @@ public class FiveImpl implements Five {
         return 0;
     }
 
-    public long[] smallest(long n) {
-        return new long[0];
+    public  long[] smallest(long n) {
+        String nStr = Long.toString(n);
+        int iDigit = 0;
+        int smallestDigit = Integer.parseInt(String.valueOf(nStr.charAt(0)));
+        for (int i = 1; i < nStr.length(); i++) {
+            int newNum = Integer.parseInt(String.valueOf(nStr.charAt(i)));
+            if (newNum < smallestDigit) {
+                smallestDigit = newNum;
+                iDigit = i;
+            }
+        }
+        nStr = nStr.substring(0,iDigit) + nStr.substring(iDigit+1);
+        if (smallestDigit == 0) {
+            return new long[]{Long.parseLong(nStr), smallestDigit, 0};
+        }
+        System.out.println(nStr);
+
+        long smallestNum = n;
+        for (int i = 0; i < nStr.length(); i++) {
+
+        }
+        return null;
+//        return new long[]{smallestNum, smallestI, smallestJ};
     }
+
+    public static String swapDigits(String str, int i, int j) {
+        char[] charArray = str.toCharArray();
+        char temp = charArray[i];
+        charArray[i] = charArray[j];
+        charArray[j] = temp;
+        return new String(charArray);
+    }
+
 
     public int artificialRain(int[] v) {
         return 0;
