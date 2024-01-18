5282a917ab15c4017ce3774d59bb2b9ca383a9f7
Anastasiia <anastasiiak55@gmail.com>
2024-01-18 23:45:54 +0200

    added throwing IllegalArgumentException in task Loking for a benefactor

--- src/main/java/org/academy/kata/implementation/nasock/SevenImpl.java
+++ src/main/java/org/academy/kata/implementation/nasock/SevenImpl.java
@@ -5,14 +5,14 @@ import org.academy.kata.Seven;
 public class SevenImpl implements Seven {
     public long newAvg(double[] arr, double navg) {
         if (arr == null || navg < 0) {
-            return -1L;
+            throw new IllegalArgumentException ();
         }
         long sum = 0L;
         for (double num : arr) {
             sum = sum + (long) num;
         }
         if (arr.length > 0 && (sum / arr.length > navg)){
-            return -1L;
+            throw new IllegalArgumentException ();
         }
         long result = (long) (navg * (arr.length+1) - sum);
         return result;
