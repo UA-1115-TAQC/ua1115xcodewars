369472b5efd7559a60da6a8581799b0758806472
Anastasiia <anastasiiak55@gmail.com>
2024-01-18 22:21:26 +0200

    solved task Looking for a benefactor

--- src/main/java/org/academy/kata/implementation/nasock/SevenImpl.java
+++ src/main/java/org/academy/kata/implementation/nasock/SevenImpl.java
@@ -4,7 +4,18 @@ import org.academy.kata.Seven;
 
 public class SevenImpl implements Seven {
     public long newAvg(double[] arr, double navg) {
-        return 0;
+        if (arr == null || navg < 0) {
+            return -1L;
+        }
+        long sum = 0L;
+        for (double num : arr) {
+            sum = sum + (long) num;
+        }
+        if (arr.length > 0 && (sum / arr.length > navg)){
+            return -1L;
+        }
+        long result = (long) (navg * (arr.length+1) - sum);
+        return result;
     }
 
     public String seriesSum(int n) {
@@ -14,4 +25,5 @@ public class SevenImpl implements Seven {
     public int whereIsHe(int p, int bef, int aft) {
         return 0;
     }
+
 }
