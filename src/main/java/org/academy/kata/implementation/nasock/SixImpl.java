bbf9e624b49d8c93a993c4705dd8c2b46c27b5bc
Anastasiia <anastasiiak55@gmail.com>
2024-01-19 14:07:01 +0200

    refactored code, changed method name to isInteger

--- src/main/java/org/academy/kata/implementation/nasock/SixImpl.java
+++ src/main/java/org/academy/kata/implementation/nasock/SixImpl.java
@@ -3,23 +3,22 @@ package org.academy.kata.implementation.nasock;
 import org.academy.kata.Six;
 
 public class SixImpl implements Six {
-    private boolean isNatural(double num){
+    private boolean isReal(double num){
         double decimalPart = (num - (int)num);
         return Double.valueOf(0L).equals(decimalPart);
     }
 
     public long findNb(long m) {
         double sqrt = Math.sqrt(m);
-        if(!isNatural(sqrt)) {
+        if(!isReal(sqrt)) {
             return -1L;
         }
 
-        double sqrtD = Math.sqrt((8 * sqrt + 1));
-        if(!isNatural(sqrtD)){
+        double sqrtDeterminant = Math.sqrt(8 * sqrt + 1);
+        if(!isReal(sqrtDeterminant)){
             return -1L;
         }
-        long result = (long) ((sqrtD - 1)/2);
-        return result;
+        return (long) (sqrtDeterminant - 1)/2;
     }
 
     public String balance(String book) {
