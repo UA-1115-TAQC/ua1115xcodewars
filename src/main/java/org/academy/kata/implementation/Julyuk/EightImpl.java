f5aa776346e9ec7e0a58fbf93730e234f33a233c
Yuliia_Ukrainets <ulaukrainec@gmail.com>
2024-01-19 16:53:29 +0200

    Optimised the solution

--- src/main/java/org/academy/kata/implementation/Julyuk/EightImpl.java
+++ src/main/java/org/academy/kata/implementation/Julyuk/EightImpl.java
@@ -42,12 +42,11 @@ public class EightImpl implements Eight {
         if (n == 1 || n == 0) {
             return false;
         } else {
-            for (int i = 2; i * i <= n; i++) {
+            for (int i = 2; i * i <= (n / 2); i++) {
                 if (n % i == 0) {
                     return false;
                 }
             }
-
         }
 
         BigInteger factorial = BigInteger.ONE;
