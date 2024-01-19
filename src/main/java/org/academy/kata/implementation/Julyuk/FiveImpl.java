f726a3020d79ba68a177e680d286191646e1ee18
Yuliia_Ukrainets <ulaukrainec@gmail.com>
2024-01-19 20:56:46 +0200

    Finished the Gap task

--- src/main/java/org/academy/kata/implementation/Julyuk/FiveImpl.java
+++ src/main/java/org/academy/kata/implementation/Julyuk/FiveImpl.java
@@ -6,8 +6,27 @@ import java.math.BigInteger;
 
 public class FiveImpl implements Five {
     public long[] gap(int g, long m, long n) {
-        return new long[0];
+        long firstPrime = 0;
+        long secondPrime = 0;
+        for (long i = m; i <= n; i++) {
+            if (isPrime(i)) {
+                firstPrime = secondPrime;
+                secondPrime = i;
+            }
+            if (secondPrime - firstPrime == g) return new long[]{firstPrime, secondPrime};
+        }
+        return null;
+        }
+    private static boolean isPrime(long n) {
+        if (n <= 2) return false;
+            for (long i = 3; i < n; i += 2) {
+                if (n % i == 0) return false;
+        }
+        return true;
     }
+
+
+
 
     public int zeros(int n) {
         return 0;
