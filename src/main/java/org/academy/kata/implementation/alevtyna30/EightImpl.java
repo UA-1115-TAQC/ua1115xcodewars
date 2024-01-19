e087f7a5c59b41a64cb7c3c3670e48f68d352001
Alevtyna Parysta <luxor952@gmail.com>
2024-01-19 16:09:50 +0100

    add Miles per gallon to kilometers per liter task

--- src/main/java/org/academy/kata/implementation/alevtyna30/EightImpl.java
+++ src/main/java/org/academy/kata/implementation/alevtyna30/EightImpl.java
@@ -3,6 +3,8 @@ package org.academy.kata.implementation.alevtyna30;
 import org.academy.kata.Eight;
 
 public class EightImpl implements Eight {
+    private static final double MILES_TO_KM = 1.609344;
+    private static final double GALLONS_TO_LITERS = 4.54609188;
     public int liters(double time) {
         return 0;
     }
@@ -12,7 +14,7 @@ public class EightImpl implements Eight {
     }
 
     public float mpgToKPM(float mpg) {
-        return 0;
+        return (float) (mpg * MILES_TO_KM / GALLONS_TO_LITERS);
     }
 
     public int[] squareOrSquareRoot(int[] array) {
