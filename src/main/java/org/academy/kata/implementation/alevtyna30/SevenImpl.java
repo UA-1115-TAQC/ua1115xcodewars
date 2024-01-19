1183f92c2aec59cf0adf5628f699188a46af105c
Alevtyna Parysta <luxor952@gmail.com>
2024-01-19 16:22:09 +0100

    add Sum of the first nth term of Series task

--- src/main/java/org/academy/kata/implementation/alevtyna30/SevenImpl.java
+++ src/main/java/org/academy/kata/implementation/alevtyna30/SevenImpl.java
@@ -8,7 +8,16 @@ public class SevenImpl implements Seven {
     }
 
     public String seriesSum(int n) {
-        return null;
+        if (n == 0) {
+            return "0.00";
+        }
+        double sum = 0.0;
+        for (int i = 0; i < n; i++) {
+            sum += 1.0 / (1 + i * 3);
+        }
+        sum = Math.round(sum * 100.0) / 100.0;
+
+        return String.format("%.2f", sum);
     }
 
     public int whereIsHe(int p, int bef, int aft) {
