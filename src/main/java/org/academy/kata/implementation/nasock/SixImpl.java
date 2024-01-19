fcdaef85c4c5d2a3483098ec1fbf5f32bcc00c0d
Anastasiia <anastasiiak55@gmail.com>
2024-01-19 02:14:07 +0200

    solvet task Build a pile of Cubes

--- src/main/java/org/academy/kata/implementation/nasock/SixImpl.java
+++ src/main/java/org/academy/kata/implementation/nasock/SixImpl.java
@@ -3,8 +3,23 @@ package org.academy.kata.implementation.nasock;
 import org.academy.kata.Six;
 
 public class SixImpl implements Six {
+    private boolean isNatural(double num){
+        double decimalPart = (num - (int)num);
+        return Double.valueOf(0L).equals(decimalPart);
+    }
+
     public long findNb(long m) {
-        return 0;
+        double sqrt = Math.sqrt(m);
+        if(!isNatural(sqrt)) {
+            return -1L;
+        }
+
+        double sqrtD = Math.sqrt((8 * sqrt + 1));
+        if(!isNatural(sqrtD)){
+            return -1L;
+        }
+        long result = (long) ((sqrtD - 1)/2);
+        return result;
     }
 
     public String balance(String book) {
@@ -30,4 +45,5 @@ public class SixImpl implements Six {
     public String stockSummary(String[] lstOfArt, String[] lstOf1stLetter) {
         return null;
     }
+    
 }
