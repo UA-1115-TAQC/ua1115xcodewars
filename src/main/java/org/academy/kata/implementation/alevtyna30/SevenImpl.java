f427ef914a351a4ca71c55e1f44e3535dd8f6d91
Alevtyna Parysta <luxor952@gmail.com>
2024-01-19 16:15:38 +0100

    add Looking for a benefactor task

--- src/main/java/org/academy/kata/implementation/alevtyna30/SevenImpl.java
+++ src/main/java/org/academy/kata/implementation/alevtyna30/SevenImpl.java
@@ -4,7 +4,17 @@ import org.academy.kata.Seven;
 
 public class SevenImpl implements Seven {
     public long newAvg(double[] arr, double navg) {
-        return 0;
+        double currentTotal = 0;
+        for (double donation : arr) {
+            currentTotal += donation;
+        }
+        long nextDonation = (long) Math.ceil(navg * (arr.length + 1) - currentTotal);
+
+        if (nextDonation <= 0) {
+            throw new IllegalArgumentException("Last donation should be a positive number.");
+        }
+
+        return nextDonation;
     }
 
     public String seriesSum(int n) {
