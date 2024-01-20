faa3cd2c22c7c8767d8728bab8129d5d8d5f7ce6
Olena <yelenastankevych@gmail.com>
2024-01-20 21:46:12 +0200

    implemented mean, variance methods in SixImpl

--- src/main/java/org/academy/kata/implementation/ystankevych/SixImpl.java
+++ src/main/java/org/academy/kata/implementation/ystankevych/SixImpl.java
@@ -2,6 +2,11 @@ package org.academy.kata.implementation.ystankevych;
 
 import org.academy.kata.Six;
 
+import java.util.Arrays;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.DoubleStream;
+
 public class SixImpl implements Six {
     public long findNb(long m) {
         return 0;
@@ -16,11 +21,27 @@ public class SixImpl implements Six {
     }
 
     public double mean(String town, String strng) {
-        return 0;
+        return getStatisticsByTown(town, strng)
+                .average()
+                .orElse(-1);
     }
 
     public double variance(String town, String strng) {
-        return 0;
+        final double average = mean(town, strng);
+        return average < 0 ? -1 : getStatisticsByTown(town, strng)
+                .boxed()
+                .collect(Collectors.averagingDouble(v -> Math.pow(v - average, 2)));
+    }
+
+    private static DoubleStream getStatisticsByTown(String town, String strng) {
+        return Pattern.compile("(?m)^" + town + ":(.*)$")
+                .matcher(strng)
+                .results()
+                .map(s -> s.group(1))
+                .flatMap(s -> Arrays.stream(s.split(","))
+                        .filter(c -> !c.isBlank())
+                        .map(c -> c.replaceAll("[^\\d.]", "")))
+                .mapToDouble(Double::parseDouble);
     }
 
     public String nbaCup(String resultSheet, String toFind) {
