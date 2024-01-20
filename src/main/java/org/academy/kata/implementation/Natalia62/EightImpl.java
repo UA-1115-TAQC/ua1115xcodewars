3855d68c280e522101725480a356ecd36622d74c
Nataliia Lazorko <natalia.lazorko.45@gmail.com>
2024-01-20 12:44:32 +0200

    Add TwoDecimalPlaces

--- src/main/java/org/academy/kata/implementation/Natalia62/EightImpl.java
+++ src/main/java/org/academy/kata/implementation/Natalia62/EightImpl.java
@@ -28,7 +28,7 @@ public class EightImpl implements Eight {
     }
 
     public double TwoDecimalPlaces(double number) {
-        return 0;
+        return Math.round(number * 100d) / 100d;
     }
 
     public int[] divisibleBy(int[] numbers, int divider) {
