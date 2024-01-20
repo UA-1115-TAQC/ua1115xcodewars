6fba8b19b15881a63cbfcc502ddfe029e5467e06
Stanislav Hrynus <stanislav.hrynus@gmail.com>
2024-01-20 23:42:41 +0200

    realized solution for kata rainfall

--- src/main/java/org/academy/kata/implementation/s_hrynus/SixImpl.java
+++ src/main/java/org/academy/kata/implementation/s_hrynus/SixImpl.java
@@ -2,7 +2,15 @@ package org.academy.kata.implementation.s_hrynus;
 
 import org.academy.kata.Six;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.DoubleStream;
+import java.util.stream.Stream;
+
 public class SixImpl implements Six {
+    private static final String FLOAT_NUMBER_ISOLATOR = "\\b(\\d+\\.\\d+)\\b";
+    private static final String LINE_SEPARATOR = "\n";
+
     public long findNb(long m) {
         return 0;
     }
@@ -16,12 +24,42 @@ public class SixImpl implements Six {
     }
 
     public double mean(String town, String strng) {
-        return 0;
+        double sumOfRainfallData = Stream.of(strng.split(LINE_SEPARATOR))
+                .filter(citiesData -> citiesData.startsWith(town + ":"))
+                .mapToDouble(SixImpl::getRainfallDataByMonths)
+                .sum();
+        return sumOfRainfallData != 0 ? sumOfRainfallData / 12 : - 1.0;
     }
 
     public double variance(String town, String strng) {
-        return 0;
+        double averageOfRainfall = mean(town, strng);
+        Pattern pattern = Pattern.compile(FLOAT_NUMBER_ISOLATOR);
+        return Stream.of(strng.split(LINE_SEPARATOR))
+                .filter(cityData -> cityData.startsWith(town + ":"))
+                .map(line -> {
+                    Matcher matcher = pattern.matcher(line);
+                    return matcher.results()
+                            .mapToDouble(match -> Double.parseDouble(match.group(1)))
+                            .toArray();
+                })
+                .flatMapToDouble(DoubleStream::of)
+                .map(value -> Math.pow(value - averageOfRainfall, 2))
+                .average().orElse(-1.0);
     }
+
+    private static double getRainfallDataByMonths(String cityData) {
+        String[] monthsData = cityData.split(",");
+        Pattern pattern = Pattern.compile(FLOAT_NUMBER_ISOLATOR);
+        double rainfallDataByMoths = 0;
+        for (String monthData : monthsData) {
+            Matcher matcher = pattern.matcher(monthData);
+            if (matcher.find()) {
+                rainfallDataByMoths += Double.parseDouble(matcher.group(1));
+            }
+        }
+        return rainfallDataByMoths;
+    }
+
 
     public String nbaCup(String resultSheet, String toFind) {
         return null;
