2343135438a61ad3b55da714b9bffe72577aa06b
Stanislav Hrynus <stanislav.hrynus@gmail.com>
2024-01-21 21:55:16 +0200

    realized solution for kata ranking nba teams

--- src/main/java/org/academy/kata/implementation/s_hrynus/SixImpl.java
+++ src/main/java/org/academy/kata/implementation/s_hrynus/SixImpl.java
@@ -2,7 +2,17 @@ package org.academy.kata.implementation.s_hrynus;
 
 import org.academy.kata.Six;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class SixImpl implements Six {
+    public static final int SCORES = 0;
+    public static final int CONCEDED = 1;
+    public static final int WIN = 2;
+    public static final int LOSE = 3;
+    public static final int DRAW = 4;
     public long findNb(long m) {
         return 0;
     }
@@ -24,7 +34,74 @@ public class SixImpl implements Six {
     }
 
     public String nbaCup(String resultSheet, String toFind) {
-        return null;
+        if (toFind.equals("")) {
+            return "";
+        }
+        if (!resultSheet.contains(toFind + " ")) {
+            return toFind + ":This team didn't play!";
+        }
+        String[] games = resultSheet.split(",");
+        int[] statistic = new int[5];
+        for (String game : games) {
+            if (game.contains(toFind)) {
+                List<Integer> gameScore = getGameScore(game);
+                String team = extractTeams(game);
+                if (isFloatingNumberPresent(game)) {
+                    return "Error(float number):" + game;
+                }
+                if ((team.startsWith(toFind) && gameScore.get(0) > gameScore.get(1))) {
+                    statistic[SCORES] += gameScore.get(0);
+                    statistic[CONCEDED] += gameScore.get(1);
+                    statistic[WIN]++;
+                }
+                if (!team.startsWith(toFind) && gameScore.get(0) < gameScore.get(1)) {
+                    statistic[SCORES] += gameScore.get(1);
+                    statistic[CONCEDED] += gameScore.get(0);
+                    statistic[WIN]++;
+                }
+                if (!team.startsWith(toFind) && gameScore.get(0) > gameScore.get(1)) {
+                    statistic[SCORES] += gameScore.get(1);
+                    statistic[CONCEDED] += gameScore.get(0);
+                    statistic[LOSE]++;
+                }
+                if (team.startsWith(toFind) && gameScore.get(0) < gameScore.get(1)) {
+                    statistic[SCORES] += gameScore.get(0);
+                    statistic[CONCEDED] += gameScore.get(1);
+                    statistic[LOSE]++;
+                }
+            }
+        }
+        return String.format("%s:W=%d;D=%d;L=%d;Scored=%d;Conceded=%d;Points=%d",
+                toFind, statistic[WIN], statistic[DRAW], statistic[LOSE], statistic[SCORES],
+                statistic[CONCEDED], calculatePoints(statistic));
+    }
+
+    private static List<Integer> getGameScore(String game) {
+        List<Integer> score = new ArrayList<>();
+        Pattern decimalPattern = Pattern.compile("\\s(\\d+)\\s");
+        Matcher decimalMatcher = decimalPattern.matcher(game + " ");
+        if (decimalMatcher.find()) {
+            score.add(Integer.parseInt(decimalMatcher.group().trim()));
+        }
+        if (decimalMatcher.find()) {
+            score.add(Integer.parseInt(decimalMatcher.group().trim()));
+        }
+        return score;
+    }
+
+    private static boolean isFloatingNumberPresent(String game) {
+        Pattern floatNumberPatter = Pattern.compile("\\s(\\d+\\.\\d+)\\s");
+        Matcher floatNumberMatcher = floatNumberPatter.matcher(game + " ");
+        return floatNumberMatcher.find();
+    }
+
+    private static String extractTeams(String game) {
+        String result = game.replaceAll("\\d+", "");
+        return result.replaceAll("\\s+", " ").trim();
+    }
+
+    private static int calculatePoints(int[] statistic) {
+        return statistic[WIN] * 3 + statistic[DRAW];
     }
 
     public String stockSummary(String[] lstOfArt, String[] lstOf1stLetter) {
