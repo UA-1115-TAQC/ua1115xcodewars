63ea954da9d243b14faed2aac0633e11c43b72c9
Nataliia Lazorko <natalia.lazorko.45@gmail.com>
2024-01-21 12:04:36 +0200

    Add wilson primes

--- src/main/java/org/academy/kata/implementation/Natalia62/EightImpl.java
+++ src/main/java/org/academy/kata/implementation/Natalia62/EightImpl.java
@@ -1,7 +1,7 @@
 package org.academy.kata.implementation.Natalia62;
 
 import org.academy.kata.Eight;
-
+import java.math.BigInteger;
 public class EightImpl implements Eight {
     public int liters(double time) {
         return 0;
@@ -36,6 +36,14 @@ public class EightImpl implements Eight {
     }
 
     public boolean am_i_wilson(double n) {
-        return false;
+        if (n <= 2) return false;
+        BigInteger factorial = BigInteger.ONE;
+        BigInteger number =  BigInteger.valueOf((long) n);
+
+        for (int i = 1; i < n; i++) {
+            factorial = factorial.multiply(BigInteger.valueOf(i));}
+        BigInteger Newfactorial = factorial.add(BigInteger.ONE);
+        BigInteger pow = number.multiply(number);
+        return Newfactorial.mod(pow).equals(BigInteger.ZERO);
     }
 }
