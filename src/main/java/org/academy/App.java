db83a23be9e63f0390067139707574bcb12627ce
Mariana Gogashvili <marianagogashvili24@gmail.com>
2024-01-22 12:48:08 +0200

    removed changes to App.js

--- src/main/java/org/academy/App.java
+++ src/main/java/org/academy/App.java
@@ -1,14 +1,9 @@
 package org.academy;
 
-import org.academy.kata.implementation.hohashvili.SixImpl;
-
 /**
  * Hello world!
  */
 public class App {
     public static void main(String[] args) {
-        SixImpl sixImpl = new SixImpl();
-        System.out.println(sixImpl.findNb(1071225));
-        System.out.println(sixImpl.findNb(91716553919377L));
     }
 }
