03c2b7d6a1e578c7812e1bb4e4a7b1d422c6e29d
Natalsa566 <makarchyk58@gmail.com>
2024-01-22 21:28:05 +0200

    Build_a_pile_of_Cubes

--- src/main/java/org/academy/kata/implementation/Natalsa566/SixImpl.java
+++ src/main/java/org/academy/kata/implementation/Natalsa566/SixImpl.java
@@ -3,10 +3,15 @@ package org.academy.kata.implementation.Natalsa566;
 import org.academy.kata.Six;
 
 public class SixImpl implements Six {
-    public long findNb(long m) {
-        return 0;
-    }
 
+    public long findNb(long m) {
+        long n = (long) Math.sqrt(2 * Math.sqrt(m));
+        if (n * (n + 1) / 2 * n * (n + 1) / 2 == m) {
+            return n;
+        } else {
+            return -1;
+        }
+    }
     public String balance(String book) {
         return null;
     }
