d267ed3c94b083785c8cb8b4904e6415a90bcb89
Mariana Gogashvili <marianagogashvili24@gmail.com>
2024-01-22 12:48:55 +0200

    removed changes to App.js

--- src/main/java/org/academy/App.java
+++ src/main/java/org/academy/App.java
@@ -1,16 +1,9 @@
 package org.academy;
 
-import org.academy.kata.implementation.hohashvili.EightImpl;
-
-import java.util.Arrays;
-
 /**
  * Hello world!
  */
 public class App {
     public static void main(String[] args) {
-        EightImpl eightImpl = new EightImpl();
-        int[] test = {1, 2, 3, 4, 5, 6};
-        System.out.println(Arrays.toString(eightImpl.divisibleBy(test, 2)));
     }
 }
