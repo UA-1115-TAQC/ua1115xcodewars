6f000812ea661c5ed922e2de2cac5ae5a2b3f48e
Mariana Gogashvili <marianagogashvili24@gmail.com>
2024-01-22 12:48:39 +0200

    removed changes to App.js

--- src/main/java/org/academy/App.java
+++ src/main/java/org/academy/App.java
@@ -1,20 +1,9 @@
 package org.academy;
 
-import org.academy.kata.implementation.hohashvili.SevenImpl;
-
 /**
  * Hello world!
  */
 public class App {
     public static void main(String[] args) {
-        SevenImpl sevenImpl = new SevenImpl();
-        double[] test = {14, 30, 5, 7, 9, 11, 15};
-        try {
-            System.out.println(sevenImpl.newAvg(test, 30));
-            System.out.println(sevenImpl.newAvg(test, 92));
-            System.out.println(sevenImpl.newAvg(test, 2));
-        } catch (IllegalArgumentException e) {
-            System.out.println(e);
-        }
     }
 }
