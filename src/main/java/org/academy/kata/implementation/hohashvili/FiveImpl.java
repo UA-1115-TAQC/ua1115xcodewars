a10bbf1925270bf5f118f5c89d8fe3f8305baa4e
Mariana Gogashvili <marianagogashvili24@gmail.com>
2024-01-22 12:43:14 +0200

    removed changes to App.js

--- src/main/java/org/academy/kata/implementation/hohashvili/FiveImpl.java
+++ src/main/java/org/academy/kata/implementation/hohashvili/FiveImpl.java
@@ -22,38 +22,8 @@ public class FiveImpl implements Five {
     }
 
     public  long[] smallest(long n) {
-        String nStr = Long.toString(n);
-        int iDigit = 0;
-        int smallestDigit = Integer.parseInt(String.valueOf(nStr.charAt(0)));
-        for (int i = 1; i < nStr.length(); i++) {
-            int newNum = Integer.parseInt(String.valueOf(nStr.charAt(i)));
-            if (newNum < smallestDigit) {
-                smallestDigit = newNum;
-                iDigit = i;
-            }
-        }
-        nStr = nStr.substring(0,iDigit) + nStr.substring(iDigit+1);
-        if (smallestDigit == 0) {
-            return new long[]{Long.parseLong(nStr), smallestDigit, 0};
-        }
-        System.out.println(nStr);
-
-        long smallestNum = n;
-        for (int i = 0; i < nStr.length(); i++) {
-
-        }
-        return null;
-//        return new long[]{smallestNum, smallestI, smallestJ};
+        return new long[0];
     }
-
-    public static String swapDigits(String str, int i, int j) {
-        char[] charArray = str.toCharArray();
-        char temp = charArray[i];
-        charArray[i] = charArray[j];
-        charArray[j] = temp;
-        return new String(charArray);
-    }
-
 
     public int artificialRain(int[] v) {
         return 0;
