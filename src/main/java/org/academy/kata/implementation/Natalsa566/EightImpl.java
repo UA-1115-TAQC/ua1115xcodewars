7cc50d16b5d2dab010bb6dbd1bfeb2672163f384
Natalsa566 <makarchyk58@gmail.com>
2024-01-22 14:39:18 +0200

    Count_of_positives_sum_of_negatives

--- src/main/java/org/academy/kata/implementation/Natalsa566/EightImpl.java
+++ src/main/java/org/academy/kata/implementation/Natalsa566/EightImpl.java
@@ -19,8 +19,28 @@ public class EightImpl implements Eight {
         return new int[0];
     }
 
-    public int[] countPositivesSumNegatives(int[] input) {
-        return new int[0];
+    @Override
+    public int[] countPositivesSumNegatives ( int[] input){
+        if (input == null || input.length == 0) {
+            return new int[0];
+        }
+            int countPositives = 0;
+            int sumNegatives = 0;
+
+               for (int num : input) {
+                  if (num > 0) {
+                     countPositives++;
+                       } else if (num < 0) {
+                            sumNegatives += num;
+            }
+        }
+        return new int[]{countPositives, sumNegatives};
+    }
+    public static void main (String[]args){
+        EightImpl eightImpl = new EightImpl();
+        int[] result = eightImpl.countPositivesSumNegatives(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15});
+        System.out.println("Count of positives: " + result[0]);
+        System.out.println("Sum of negatives: " + result[1]);
     }
 
     public int stringToNumber(String str) {
