949b7f13b6221c904cf52c2d96084c55fc0d4003
Natalsa566 <makarchyk58@gmail.com>
2024-01-22 14:45:43 +0200

    Convert_a_String_to_a_Number

--- src/main/java/org/academy/kata/implementation/Natalsa566/EightImpl.java
+++ src/main/java/org/academy/kata/implementation/Natalsa566/EightImpl.java
@@ -24,7 +24,19 @@ public class EightImpl implements Eight {
     }
 
     public int stringToNumber(String str) {
-        return 0;
+        return Integer.parseInt(str);
+    }
+    public static void main(String[] args) {
+        Eight converter = new EightImpl();
+        String str1 = "1234";
+        String str2 = "605";
+        String str3 = "1405";
+        String str4 = "-7";
+
+        System.out.println(converter.stringToNumber(str1));
+        System.out.println(converter.stringToNumber(str2));
+        System.out.println(converter.stringToNumber(str3));
+        System.out.println(converter.stringToNumber(str4));
     }
 
     public double TwoDecimalPlaces(double number) {
