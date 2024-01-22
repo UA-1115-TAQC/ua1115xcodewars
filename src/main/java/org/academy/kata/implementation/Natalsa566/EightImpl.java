14d58beb20908aa6b204edd9164691e889c5a483
Natalsa566 <makarchyk58@gmail.com>
2024-01-22 15:46:01 +0200

    Find_numbers_which_are_divisible_by_given_number

--- src/main/java/org/academy/kata/implementation/Natalsa566/EightImpl.java
+++ src/main/java/org/academy/kata/implementation/Natalsa566/EightImpl.java
@@ -1,6 +1,9 @@
 package org.academy.kata.implementation.Natalsa566;
 
 import org.academy.kata.Eight;
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 
 public class EightImpl implements Eight {
     public int liters(double time) {
@@ -32,9 +35,25 @@ public class EightImpl implements Eight {
     }
 
     public int[] divisibleBy(int[] numbers, int divider) {
-        return new int[0];
+        List<Integer> result = new ArrayList<>();
+             for (int number : numbers) {
+                if (number % divider == 0) {
+                    result.add(number);
+            }
     }
-
+                int[] resultArray = new int[result.size()];
+                for (int i = 0; i < result.size(); i++) {
+                    resultArray[i] = result.get(i);
+                     }
+                         return resultArray;
+}
+           public static void main(String[] args) {
+                 EightImpl eightImpl = new EightImpl();
+                 int[] inputArray = {1, 2, 3, 4, 5, 6};
+                 int divisor = 2;
+                 int[] result = eightImpl.divisibleBy(inputArray, divisor);
+                 System.out.println(Arrays.toString(result));
+}
     public boolean am_i_wilson(double n) {
         return false;
     }
