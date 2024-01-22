906d7f19ca541790ee5895a6f6ebe8cbe20e1bf1
Mariana Gogashvili <marianagogashvili24@gmail.com>
2024-01-22 12:50:30 +0200

    removed changes to App.js

--- src/main/java/org/academy/App.java
+++ src/main/java/org/academy/App.java
@@ -9,10 +9,5 @@ import java.util.Arrays;
  */
 public class App {
     public static void main(String[] args) {
-        EightImpl eightImpl = new EightImpl();
-        int[] test = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15};
-//        int[] test = {};
-        int[] result = eightImpl.countPositivesSumNegatives(test);
-        System.out.println(Arrays.toString(result));
     }
 }
