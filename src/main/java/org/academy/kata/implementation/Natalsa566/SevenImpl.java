554bf9d03bdafeb8a864e5a64fd521380ff67435
Natalsa566 <makarchyk58@gmail.com>
2024-01-22 20:01:01 +0200

    remove main

--- src/main/java/org/academy/kata/implementation/Natalsa566/SevenImpl.java
+++ src/main/java/org/academy/kata/implementation/Natalsa566/SevenImpl.java
@@ -6,10 +6,14 @@ public class SevenImpl implements Seven {
     public long newAvg(double[] arr, double navg) {
         return 0;
     }
-
+@Override
     public String seriesSum(int n) {
-        return null;
+    double sum = 0;
+        for (int i = 0; i < n; i++) {
+        sum += 1.0 / (1 + i * 3);
     }
+        return String.format("%.2f", sum);
+}
 
     public int whereIsHe(int p, int bef, int aft) {
         return 0;
