84bd2ee9922bd12884c71626e7a39e2dad75e211
Mariana Gogashvili <marianagogashvili24@gmail.com>
2024-01-22 12:40:56 +0200

    removed changes to App.js

--- src/main/java/org/academy/App.java
+++ src/main/java/org/academy/App.java
@@ -1,16 +1,9 @@
 package org.academy;
 
-import org.academy.kata.implementation.hohashvili.FiveImpl;
-
 /**
  * Hello world!
  */
 public class App {
     public static void main(String[] args) {
-        FiveImpl fiveImpl = new FiveImpl();
-        System.out.println(fiveImpl.solve(2.00)); // 5.000000000000e-01
-        System.out.println(fiveImpl.solve(4.00)); // 6.096117967978e-01
-        System.out.println(fiveImpl.solve(5.00)); // 6.417424305044e-01
-
     }
 }
