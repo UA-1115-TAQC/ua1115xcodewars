79e292345c278bb3092adae27095a259546397af
Natalsa566 <makarchyk58@gmail.com>
2024-01-22 13:00:55 +0200

    // Conversion factors

--- src/main/java/org/academy/kata/implementation/Natalsa566/EightImpl.java
+++ src/main/java/org/academy/kata/implementation/Natalsa566/EightImpl.java
@@ -12,7 +12,19 @@ public class EightImpl implements Eight {
     }
 
     public float mpgToKPM(float mpg) {
-        return 0;
+        float gallonsToLiters = 4.54609188f;
+        float milesToKilometers = 1.609344f;
+        float kpm = (mpg * milesToKilometers) / gallonsToLiters;
+        kpm = Math.round(kpm * 100.0) / 100.0f;
+        return kpm;
+    }
+    public static void main(String[] args) {
+        Eight converter = new EightImpl();
+        float mpg = 35.0f;
+
+        float kpm = converter.mpgToKPM(mpg);
+
+        System.out.println(mpg + " mpg is equal to " + kpm + " kpm");
     }
 
     public int[] squareOrSquareRoot(int[] array) {
