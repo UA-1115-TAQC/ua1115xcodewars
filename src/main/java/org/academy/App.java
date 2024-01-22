f39d32ba2eabb9875e72a8c6ac9877fbd0ba1f13
Mariana Gogashvili <marianagogashvili24@gmail.com>
2024-01-22 12:44:23 +0200

    removed changes to App.js

--- src/main/java/org/academy/App.java
+++ src/main/java/org/academy/App.java
@@ -1,17 +1,9 @@
 package org.academy;
 
-import org.academy.kata.implementation.hohashvili.FiveImpl;
-
-import java.math.BigInteger;
-
 /**
  * Hello world!
  */
 public class App {
     public static void main(String[] args) {
-
-        FiveImpl fiveImpl = new FiveImpl();
-        System.out.println(fiveImpl.perimeter(BigInteger.valueOf(7)));
-        System.out.println(fiveImpl.perimeter(BigInteger.valueOf(5)));
     }
 }
