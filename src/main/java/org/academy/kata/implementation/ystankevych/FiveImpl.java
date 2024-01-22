882bd1e7c7022a1deebdfbc06a1f73ee8464ca0f
Olena <yelenastankevych@gmail.com>
2024-01-22 22:18:17 +0200

    implemented gap method in FiveImpl class

--- src/main/java/org/academy/kata/implementation/ystankevych/FiveImpl.java
+++ src/main/java/org/academy/kata/implementation/ystankevych/FiveImpl.java
@@ -1,12 +1,34 @@
 package org.academy.kata.implementation.ystankevych;
 
 import org.academy.kata.Five;
-
 import java.math.BigInteger;
 
 public class FiveImpl implements Five {
     public long[] gap(int g, long m, long n) {
-        return new long[0];
+        if (g < 2 || g % 2 != 0 || m <= 2 || n < m) {
+            return null;
+        }
+
+        long temp = 0;
+
+        for (long i = m % 2 == 0 ? m + 1 : m; i < n; i += 2) {
+            if (isPrime(i)) {
+                if (i - temp == g) {
+                    return new long[] {temp, i};
+                }
+                temp = i;
+            }
+        }
+        return null;
+    }
+
+    private static boolean isPrime(long value) {
+        for (int i = 2; i <= Math.sqrt(value); i++) {
+            if (value % i == 0) {
+                return false;
+            }
+        }
+        return true;
     }
 
     public int zeros(int n) {
