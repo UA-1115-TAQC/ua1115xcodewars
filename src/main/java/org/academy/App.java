8688c72669ccbeb470d5250b437a32053c899322
Mariana Gogashvili <marianagogashvili24@gmail.com>
2024-01-22 12:49:47 +0200

    removed changes to App.js

--- src/main/java/org/academy/App.java
+++ src/main/java/org/academy/App.java
@@ -1,16 +1,9 @@
 package org.academy;
 
-import org.academy.kata.implementation.hohashvili.EightImpl;
-
 /**
  * Hello world!
  */
 public class App {
     public static void main(String[] args) {
-        EightImpl eightImpl = new EightImpl();
-        System.out.println(eightImpl.am_i_wilson(5));
-        System.out.println(eightImpl.am_i_wilson(13));
-        System.out.println(eightImpl.am_i_wilson(563));
-        System.out.println(eightImpl.am_i_wilson(10));
     }
 }
