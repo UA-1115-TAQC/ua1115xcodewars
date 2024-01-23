6a06b714d590c0562353b1303b866932f437c1e0
Oleg Cherepnin <cherepnin11@gmail.com>
2024-01-23 16:20:51 +0200

    [Gap in Primes]: complete task

--- src/main/java/org/academy/kata/implementation/Oleghon/FiveImpl.java
+++ src/main/java/org/academy/kata/implementation/Oleghon/FiveImpl.java
@@ -3,10 +3,19 @@ package org.academy.kata.implementation.Oleghon;
 import org.academy.kata.Five;
 
 import java.math.BigInteger;
+import java.util.function.LongPredicate;
+import java.util.stream.LongStream;
 
 public class FiveImpl implements Five {
     public long[] gap(int g, long m, long n) {
-        return new long[0];
+        LongPredicate prime = l -> BigInteger.valueOf(l).isProbablePrime(9);
+        return LongStream.range(m, n - g)
+                .filter(i -> prime.test(i)
+                        && prime.test(i + g)
+                        && LongStream.range(i + 1, i + g).noneMatch(prime))
+                .mapToObj(i -> new long[]{i, i + g})
+                .findFirst()
+                .orElse(null);
     }
 
     public int zeros(int n) {
