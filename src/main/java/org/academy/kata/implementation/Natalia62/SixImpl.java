bedcbeed4330eae844233fe3377d5afde6392a37
Nataliia Lazorko <natalia.lazorko.45@gmail.com>
2024-01-24 22:05:06 +0200

    Add Ranking NBA teams

--- src/main/java/org/academy/kata/implementation/Natalia62/SixImpl.java
+++ src/main/java/org/academy/kata/implementation/Natalia62/SixImpl.java
@@ -2,6 +2,10 @@ package org.academy.kata.implementation.Natalia62;
 
 import org.academy.kata.Six;
 
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class SixImpl implements Six {
     public long findNb(long m) {
         long sum = 0;
@@ -46,7 +50,45 @@ public class SixImpl implements Six {
     }
 
     public String nbaCup(String resultSheet, String toFind) {
-        return null;
+        if (toFind.isEmpty()) return "";
+        if(!resultSheet.contains(toFind + " ")) return toFind + ":This team didn't play!";
+
+        String[] nba = resultSheet.split(",");
+        Pattern pattern = Pattern.compile("(.+)\\s+(\\d+\\.?\\d*)\\s+(.+)\\s+(\\d+\\.?\\d*)");
+        int w = 0;
+        int d = 0;
+        int l = 0;
+        int scored = 0;
+        int conceded = 0;
+        for (String value : nba) {
+            if (value.contains(toFind)) {
+                Matcher matcher = pattern.matcher(value);
+                String firstScore = "";
+                String secondScore = "";
+                if (matcher.find()) {
+                    if (matcher.group(2).contains(".") || matcher.group(4).contains(".") )
+                        return "Error(float number):" + value;
+                    if (matcher.group(1).equals(toFind)) {
+                        firstScore = matcher.group(2);
+                        secondScore = matcher.group(4);
+                    } else {
+                        firstScore = matcher.group(4);
+                        secondScore = matcher.group(2);
+                    }
+
+                    int firstScoreInt = Integer.parseInt(firstScore);
+                    int secondScoreInt = Integer.parseInt(secondScore);
+                    scored += firstScoreInt;
+                    conceded += secondScoreInt;
+
+                    if (firstScoreInt > secondScoreInt) w++;
+                    else if (firstScoreInt == secondScoreInt) d++;
+                    else l++;
+                }
+            }
+        }
+        int points = w * 3 + d;
+        return toFind + ":W=" + w + ";D=" + d + ";L=" + l + ";Scored=" + scored + ";Conceded=" + conceded + ";Points=" + points;
     }
 
     public String stockSummary(String[] lstOfArt, String[] lstOf1stLetter) {
