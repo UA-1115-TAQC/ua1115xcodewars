b35e88e996399c2a1bc7f91c85a125e3f9e35201
KhrystynaPavlikovska <KhrystynaPavlikovska@users.noreply.github.com>
2024-01-25 01:18:50 +0200

    codewars - (Rainfall) task

--- src/main/java/org/academy/kata/implementation/KhrystynaPavlikovska/SixImpl.java
+++ src/main/java/org/academy/kata/implementation/KhrystynaPavlikovska/SixImpl.java
@@ -2,6 +2,9 @@ package org.academy.kata.implementation.KhrystynaPavlikovska;
 
 import org.academy.kata.Six;
 
+
+import java.util.Arrays;
+
 public class SixImpl implements Six {
     public long findNb(long m) {
         return 0;
@@ -16,11 +19,34 @@ public class SixImpl implements Six {
     }
 
     public double mean(String town, String strng) {
-        return 0;
+        String[] townRecords = strng.split("\n");
+
+        for (String record : townRecords) {
+            if (record.startsWith(town + ":")) {
+
+                String[] rainfallValues = record.replaceAll("[^0-9.,]+", "").split(",");
+                double[] rainfallArray = Arrays.stream(rainfallValues).mapToDouble(Double::parseDouble).toArray();
+                return Arrays.stream(rainfallArray).average().orElse(-1.0);
+            }
+        }
+
+        return -1.0;
     }
 
     public double variance(String town, String strng) {
-        return 0;
+        String[] townRecords = strng.split("\n");
+
+        for (String record : townRecords) {
+            if (record.startsWith(town + ":")) {
+
+                String[] rainfallValues = record.replaceAll("[^0-9.,]+", "").split(",");
+                double[] rainfallArray = Arrays.stream(rainfallValues).mapToDouble(Double::parseDouble).toArray();
+                double mean = Arrays.stream(rainfallArray).average().orElse(-1.0);
+                return Arrays.stream(rainfallArray).map(x -> Math.pow(x - mean, 2)).average().orElse(-1.0);
+            }
+        }
+
+        return -1.0;
     }
 
     public String nbaCup(String resultSheet, String toFind) {
