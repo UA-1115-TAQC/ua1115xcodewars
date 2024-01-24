f16bcf05f155dfe97031a99022edb487dc937108
Natalsa566 <makarchyk58@gmail.com>
2024-01-25 01:02:16 +0200

    Bookseller

--- src/main/java/org/academy/kata/implementation/Natalsa566/SixImpl.java
+++ src/main/java/org/academy/kata/implementation/Natalsa566/SixImpl.java
@@ -2,6 +2,9 @@ package org.academy.kata.implementation.Natalsa566;
 
 import org.academy.kata.Six;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class SixImpl implements Six {
     public long findNb(long m) {
         return 0;
@@ -28,6 +31,27 @@ public class SixImpl implements Six {
     }
 
     public String stockSummary(String[] lstOfArt, String[] lstOf1stLetter) {
-        return null;
+        if (lstOfArt.length == 0 || lstOf1stLetter.length == 0) {
+            return "";
+        }
+        Map<String, Integer> categorySumMap = new HashMap<>();
+        for (String art : lstOfArt) {
+            String[] parts = art.split(" ");
+            String category = parts[0].substring(0, 1);
+
+            if (categorySumMap.containsKey(category)) {
+                categorySumMap.put(category, categorySumMap.get(category) + Integer.parseInt(parts[1]));
+            } else {
+                categorySumMap.put(category, Integer.parseInt(parts[1]));
+            }
+        }
+        StringBuilder result = new StringBuilder();
+        for (String letter : lstOf1stLetter) {
+            int sum = categorySumMap.getOrDefault(letter, 0);
+            result.append("(").append(letter).append(" : ").append(sum).append(") - ");
+        }
+        result.setLength(result.length() - 3);
+
+        return result.toString();
     }
 }
