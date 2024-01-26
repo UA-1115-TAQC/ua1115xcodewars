5a9dec3b8c4c85d87df5f630f87a9d999ff762cc
Liubomyr Halamaha <galamagal86@gmail.com>
2024-01-26 16:25:41 +0200

    fix

--- src/main/java/org/academy/kata/implementation/Natalsa566/SevenImpl.java
+++ src/main/java/org/academy/kata/implementation/Natalsa566/SevenImpl.java
@@ -1,23 +1,22 @@
 package org.academy.kata.implementation.Natalsa566;
 
 import org.academy.kata.Seven;
-import java.lang.IllegalArgumentException;
-import java.lang.Math;
+
 public class SevenImpl implements Seven {
     public long newAvg(double[] arr, double navg) {
         double currentSum = 0;
         for (double donation : arr) {
             currentSum += donation;
         }
-             double currentAvg = currentSum / arr.length;
-                 if (navg <= currentAvg) {
-                    throw new IllegalArgumentException("The target average should be greater than the current average.");
-                 }
-                     long requiredDonation = (long) Math.ceil((navg * (arr.length + 1)) - currentSum);
-                           if (requiredDonation <= 0) {
-                               throw new IllegalArgumentException("The required donation should be a positive value.");
+        double currentAvg = currentSum / arr.length;
+        if (navg <= currentAvg) {
+            throw new IllegalArgumentException("The target average should be greater than the current average.");
         }
-                                 return requiredDonation;
+        long requiredDonation = (long) Math.ceil((navg * (arr.length + 1)) - currentSum);
+        if (requiredDonation <= 0) {
+            throw new IllegalArgumentException("The required donation should be a positive value.");
+        }
+        return requiredDonation;
     }
 
     public String seriesSum(int n) {
@@ -27,8 +26,7 @@ public class SevenImpl implements Seven {
         }
         return String.format("%.2f", sum);
     }
-        return String.format("%.2f", sum);
-}
+
 
     public int whereIsHe(int p, int bef, int aft) {
         return 0;
