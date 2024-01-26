66131928b0f614b63e3c92ea49e0b9efe358051b
Liubomyr Halamaha <galamagal86@gmail.com>
2024-01-26 16:16:30 +0200

    mearge main

--- src/main/java/org/academy/kata/implementation/hohashvili/EightImpl.java
+++ src/main/java/org/academy/kata/implementation/hohashvili/EightImpl.java
@@ -51,7 +51,7 @@ public class EightImpl implements Eight {
         return new int[0];
     }
 
-    static double factorial(double n)
+    private double factorial(double n)
     {
         if (n == 0)
             return 1;
