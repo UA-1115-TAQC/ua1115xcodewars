d9056fdc9fe33c1cd656a088b51436809471f00a
Oleg Cherepnin <cherepnin11@gmail.com>
2024-01-30 17:38:25 +0200

    [eight-impl-test]: init EightImplTest class

---
+++ src/test/java/org/academy/kata/implementation/EightImplTest.java
@@ -0,0 +1,12 @@
+package org.academy.kata.implementation;
+
+import org.academy.kata.Eight;
+import org.testng.annotations.BeforeClass;
+
+public class EightImplTest {
+    Eight eight;
+
+    @BeforeClass
+    void setUp() {
+    }
+}
