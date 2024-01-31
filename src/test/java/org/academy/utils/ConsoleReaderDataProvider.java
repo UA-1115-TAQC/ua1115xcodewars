8e03bcfb77a4cb77211651e8a81749f49803a696
Yuliia_Ukrainets <ulaukrainec@gmail.com>
2024-01-31 21:46:13 +0200

    Added positive and negative tests for the readDoublearr method

--- src/test/java/org/academy/utils/ConsoleReaderDataProvider.java
+++ src/test/java/org/academy/utils/ConsoleReaderDataProvider.java
@@ -18,18 +18,22 @@ public class ConsoleReaderDataProvider {
     @DataProvider(name = "validDataForDoubleArr")
     private Object[][] validDataForDoubleArr(){
         return new Object[][]{
-               // {"43653664", new BigInteger("43653664")},
-              //  {"67648656565474858", new BigInteger("67648656565474858")},
-               // {"9597957656756756788655685656", new BigInteger("9597957656756756788655685656")},
+                {"1 2 3", new double[]{1.0, 2.0, 3.0}},
+                {"10.5 4.2 -2.1", new double[]{10.5, 4.2, -2.1}},
+                {"  5    6.7   ", new double[]{5.0, 6.7}},
+                {"0", new double[]{0.0}},
         };
     }
 
     @DataProvider(name = "invalidDataForDoubleArr")
     private Object[][] invalidDataForDoubleArr(){
         return new Object[][]{
-                // {"43653664", new BigInteger("43653664")},
-                //  {"67648656565474858", new BigInteger("67648656565474858")},
-                // {"9597957656756756788655685656", new BigInteger("9597957656756756788655685656")},
+                {"a b c"},
+                {"1.2.3"},
+                {"5  hello"},
+                {" "},
+                {""},
+                {"10,5 4,2 -2,1"}
         };
     }
 }
--- src/test/java/org/academy/utils/ConsoleReaderTest.java
+++ src/test/java/org/academy/utils/ConsoleReaderTest.java
@@ -1,6 +1,7 @@
 package org.academy.utils;
 
 import org.academy.utils.ConsoleReader;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.io.ByteArrayInputStream;
@@ -18,9 +19,18 @@ public class ConsoleReaderTest {
     }
 
     @Test(dataProvider = "validDataForDoubleArr", dataProviderClass = ConsoleReaderDataProvider.class)
-    public void testReadDoubleArr(String inputString, double[] expected){
+    public void testReadDoubleArr_validInputs(String inputString, double[] expected){
         System.setIn(new ByteArrayInputStream(inputString.getBytes()));
         ConsoleReader reader = new ConsoleReader();
-        assertEquals(reader.readBigInteger(), expected);
+        assertEquals(reader.readDoubleArr(), expected);
+    }
+
+    @Test(dataProvider = "invalidDataForDoubleArr", dataProviderClass = ConsoleReaderDataProvider.class)
+    public void testReadDoubleArr_invalidInputs(String inputString){
+        System.setIn(new ByteArrayInputStream(inputString.getBytes()));
+        ConsoleReader reader = new ConsoleReader();
+        Assert.expectThrows(NullPointerException.class, () -> {
+            reader.readDoubleArr();
+        });
     }
 }
