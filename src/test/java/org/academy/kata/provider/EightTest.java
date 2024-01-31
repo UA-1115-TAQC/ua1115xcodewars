15b9970485ba7262a0a04b492b403c656cee7f05
Mariana Gogashvili <marianagogashvili24@gmail.com>
2024-01-31 23:17:20 +0100

    liters test

--- src/test/java/org/academy/kata/provider/EightTest.java
+++ src/test/java/org/academy/kata/provider/EightTest.java
@@ -1,8 +1,8 @@
 package org.academy.kata.provider;
 
 import org.academy.kata.Eight;
-import org.testng.Assert;
 import org.testng.annotations.Test;
+
 import static org.testng.Assert.assertEquals;
 
 public class EightTest {
@@ -14,6 +14,11 @@ public class EightTest {
     @Test (dataProvider = "VolumeOfCuboid_validInputs", dataProviderClass = EightDataProvider.class)
     public void volumeOfCuboid_ValidInputsTest(Eight impl, double length, double width, double height, double expectedVolume) {
         assertEquals((expectedVolume - impl.getVolumeOfCuboid(length,width,height) <= 0.0001),true);
+    }
+
+    @Test (dataProvider = "liters_test", dataProviderClass = EightDataProvider.class)
+    public void liters_ValidInputsTest(Eight impl, double time, double expectedLiters) {
+        assertEquals((expectedLiters - impl.liters(time) <= 0.0001),true);
     }
 
 }
