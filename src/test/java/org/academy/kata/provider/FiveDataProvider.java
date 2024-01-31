dc511d6af97741974ffdbb1d632454282e6a804e
Oleg Cherepnin <cherepnin11@gmail.com>
2024-01-31 14:52:11 +0200

    create data provider

---
+++ src/test/java/org/academy/kata/provider/FiveDataProvider.java
@@ -0,0 +1,21 @@
+package org.academy.kata.provider;
+
+import org.testng.annotations.DataProvider;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FiveDataProvider extends AbstractDataProvider {
+
+
+    @DataProvider(name = "gap")
+    public static Object[][] gapData() {
+        List<Object[]> inputs = new ArrayList<>();
+        inputs.add(new Object[]{10, 300, 400, new long[]{337, 347}}); // input1, input2, input3, result
+        inputs.add(new Object[]{2, 100, 110, new long[]{101, 103}});  // input1, input2, input3, result
+        inputs.add(new Object[]{4, 100, 110, new long[]{103, 107}});  // input1, input2, input3, result
+        inputs.add(new Object[]{6, 100, 110, null});                  // input1, input2, input3, result
+
+        return getDataThreeParams(inputs, FIVES);
+    }
+}
