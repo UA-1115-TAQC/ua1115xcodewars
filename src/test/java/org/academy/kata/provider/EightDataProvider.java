e86bb14bb9149705bca0de08b9e7d9724671e133
Olena <yelenastankevych@gmail.com>
2024-01-31 17:38:19 +0200

    upgrade AbstractDataProvider

--- src/test/java/org/academy/kata/provider/EightDataProvider.java
+++ src/test/java/org/academy/kata/provider/EightDataProvider.java
@@ -3,18 +3,19 @@ package org.academy.kata.provider;
 import org.testng.annotations.DataProvider;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class EightDataProvider extends AbstractDataProvider {
 
     @DataProvider(name = "stringToNumber")
-    public static Object[][] stringToNumberData() {
+    public static Iterator<Object[]> stringToNumberData() {
         List<Object[]> inputs = new ArrayList<>();
         inputs.add(new Object[]{"90", 90});
         inputs.add(new Object[]{"645", 645}); // input, result
         inputs.add(new Object[]{"-7", -7}); // input1, input2, result
 
-        return getDataOneParam(inputs, FIVES);
+        return combineData(inputs, FIVES);
     }
 }
 
