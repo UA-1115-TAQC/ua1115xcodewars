521fc61430f9b50d8704245a38210d79ce5100fb
KhrystynaPavlikovska <34419998+KhrystynaPavlikovska@users.noreply.github.com>
2024-02-02 10:33:49 +0200

    Fixed (Gap in Primes) task (#314)

--- src/main/java/org/academy/kata/implementation/KhrystynaPavlikovska/FiveImpl.java
+++ src/main/java/org/academy/kata/implementation/KhrystynaPavlikovska/FiveImpl.java
@@ -2,11 +2,47 @@ package org.academy.kata.implementation.KhrystynaPavlikovska;
 
 import org.academy.kata.Five;
 
+import java.util.Arrays;
 import java.math.BigInteger;
 
 public class FiveImpl implements Five {
     public long[] gap(int g, long m, long n) {
-        return new long[0];
+
+        long[] result = new long[2];
+
+        for (long i = m; i <= n; i++) {
+            if (isPrime(i)) {
+                long nextPrime = findNextPrime(i + 1, n);
+                if (nextPrime - i == g) {
+                    result[0] = i;
+                    result[1] = nextPrime;
+                    return result;
+                }
+            }
+        }
+
+        return null;
+    }
+
+    private static boolean isPrime(long num) {
+        if (num < 2) {
+            return false;
+        }
+        for (long i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static long findNextPrime(long start, long limit) {
+        for (long i = start; i <= limit; i++) {
+            if (isPrime(i)) {
+                return i;
+            }
+        }
+        return -1;
     }
 
     public int zeros(int n) {
